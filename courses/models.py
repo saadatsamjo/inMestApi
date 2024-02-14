448d46b9761b23592da62d41de976b68837f0640
saadatsamjo <65956764+saadatsamjo@users.noreply.github.com>
2024-02-14 13:56:53 +0000

    courses app, model added

---
+++ courses/models.py
@@ -0,0 +1,16 @@
+# courses/models.py
+
+from django.db import models
+
+class Course(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    is_active = models.BooleanField(default=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
